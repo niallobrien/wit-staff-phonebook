c5d262f34ef3ae05c4285ba994d8c03b754c3ad1
Niall O'Brien <niall@emagine.ie>
2014-07-08 03:03:00 +0100

    Adds localstore support.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,24 @@
 angular.module('WITPhoneApp.controllers', [])
 
     .controller('StaffCtrl', function ($scope, StaffList) {
-        StaffList.all().then(function (response) {
-            $scope.staff = response.data;
-        });
+        var staffList = angular.fromJson(window.localStorage['staff_list']);
+        if (staffList) {
+            $scope.staff = staffList;
+        } else {
+            StaffList.all().success(function (response) {
+                StaffList.save(angular.toJson(response));
+                $scope.staff = response;
+            });
+        }
     })
 
     .controller('StaffDetailCtrl', function ($scope, $stateParams, StaffList) {
-        StaffList.get($stateParams.staffId).then(function(response) {
-            $scope.person = response;
-        });
+        staffList = angular.fromJson(window.localStorage['staff_list']);
+        if (staffList) {
+            $scope.person = StaffList.get($stateParams.staffId, staffList);
+        } else {
+            StaffList.all().success(function (response) {
+                $scope.person = StaffList.get($stateParams.staffId, response);
+            })
+        }
     });
--- www/js/services.js
+++ www/js/services.js
@@ -7,29 +7,23 @@ angular.module('WITPhoneApp.services', [])
  * A simple example service that returns some data.
  */
     .factory('StaffList', function ($http) {
-        var staffList = [],
-            staffMember = {};
 
         return {
             all: function () {
-                // returns promise object
                 return staffList = $http.get('http://www.wit.ie/api/get_channel_entries?channel_id=60', {
                     cache: true
-                });
+                })
             },
-            get: function (staffId) {
-                // fetch the data first
-                if (staffList == '') {
-                    staffList = this.all();
-                }
+            get: function (staffId, data) {
                 // search through staffList so not to make another http call
-                return staffList.then(function(response) {
-                    for (i = 0; i < response.data.length; i++) {
-                        if (staffId === response.data[i].entry_id) {
-                            return response.data[i];
+                    for (i = 0; i < data.length; i++) {
+                        if (staffId === data[i].entry_id) {
+                            return data[i];
                         }
-                    }
-                });
+                }
+            },
+            save: function (data) {
+                window.localStorage['staff_list'] = data;
             }
         }
     });
