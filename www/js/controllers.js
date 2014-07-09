4676ac17e61a1bdd3329ef42acb96bbe2e395105
Niall O'Brien <niall@emagine.ie>
2014-07-09 01:03:40 +0100

    Replaces ng-repeat with collection-repeat.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,13 +3,20 @@ angular.module('WITPhoneApp.controllers', [])
     .controller('StaffCtrl', function ($scope, StaffList) {
         var staffList = angular.fromJson(window.localStorage['staff_list']);
         if (staffList) {
-            $scope.staff = staffList;
+            $scope.staff = [];
+            for (var i = 0; i < staffList.length; i++) {
+                $scope.staff.push(staffList[i]);
+            }
         } else {
             StaffList.all().success(function (response) {
                 StaffList.save(angular.toJson(response));
                 $scope.staff = response;
             });
         }
+        $scope.getItemHeight = function(item, index) {
+            //Make evenly indexed items be 10px taller, for the sake of example
+            return (index % 2) === 0 ? 50 : 60;
+        };
     })
 
     .controller('StaffDetailCtrl', function ($scope, $stateParams, StaffList) {
