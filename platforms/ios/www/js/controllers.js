48ba4305d762e10ca0cde71e8a2b322168639a2b
Niall O'Brien <niall@emagine.ie>
2014-07-15 02:48:51 +0100

    Adds refresh icon & functionality.

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,14 +1,45 @@
 angular.module('WITPhoneApp.controllers', [])
 
-    .controller('StaffCtrl', function ($scope, StaffList) {
+    .controller('StaffCtrl', function (StaffList, $scope, $timeout) {
         var staffList = angular.fromJson(window.localStorage['staff_list']);
-        if (staffList) {
-            $scope.staff = [];
-            for (var i = 0; i < staffList.length; i++) {
-                $scope.staff.push(staffList[i]);
-                //console.log($scope.staff);
+
+        // self-executing function
+        var saveStaffList = (function saveStaffList() {
+            if (staffList) {
+                $scope.staff = [];
+                for (var i = 0; i < staffList.length; i++) {
+                    $scope.staff.push(staffList[i]);
+                }
+            } else {
+                getStaffList();
             }
-        } else {
+            return saveStaffList;
+        }());
+
+        // pull-down to refresh - get the list, then save it first
+        $scope.doRefresh = function doRefresh() {
+            StaffList.update($scope.staff).then(function (newStaffList) {
+                // Promise was resolved. List needs updating!
+                $scope.staffList = newStaffList;
+            }, function () {
+                // Promise was rejected. No update needed!
+                $scope.flash = {
+                    hasMessage: true,
+                    message: "No updates available."
+                };
+
+                $timeout(function () {
+                    $scope.flash = { hasMessage: false};
+                    console.log($scope.flash);
+                }, 2000);
+
+            }).finally(function () {
+                // Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
+
+        function getStaffList() {
             StaffList.all().success(function (response) {
                 StaffList.save(angular.toJson(response));
                 $scope.staff = response;
--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -6,24 +6,80 @@ angular.module('WITPhoneApp.services', [])
 /**
  * A simple example service that returns some data.
  */
-    .factory('StaffList', function ($http) {
+    .factory('StaffList', function ($http, $q) {
 
         return {
             all: function () {
-                return staffList = $http.get('http://www.wit.ie/api/get_channel_entries?channel_id=60', {
+                return $http.get('http://www.wit.ie/api/get_channel_entries?channel_id=60', {
                     cache: true
                 })
             },
             get: function (staffId, data) {
                 // search through staffList so not to make another http call
-                    for (i = 0; i < data.length; i++) {
-                        if (staffId === data[i].entry_id) {
-                            return data[i];
-                        }
+                for (i = 0; i < data.length; i++) {
+                    if (staffId === data[i].entry_id) {
+                        return data[i];
+                    }
                 }
             },
             save: function (data) {
                 window.localStorage['staff_list'] = data;
+            },
+            update: function(staffList) {
+                // Create a new deferred object
+                var deferred = $q.defer();
+                // get local last updated timestamp
+                var lastUpdatedLocal = "";
+                for (i=0; i < staffList.length; i++) {
+                    // get largest (most recent) timestamp
+                    if(staffList[i].entry_date > lastUpdatedLocal) {
+                        lastUpdatedLocal = staffList[i].entry_date;
+                    }
+                }
+
+                // Make a call to the remote api
+                $http.get('http://www.wit.ie/api/get_channel_entries?channel_id=60&order_by=entry_date&limit=1').then(function(response) {
+
+                    // Get the remote last updated
+                    // time from the response
+                    var lastUpdatedRemote = response.data[0].entry_date;
+
+                    // Check if local or remote is larger
+                    // You probably need to modify this
+                    // depending on your datetime formatting.
+                    // I am assuming an epoch string.
+                    if (lastUpdatedRemote > lastUpdatedLocal) {
+                        // Resolve the deferred and return
+                        // the data so that the controller can
+                        // use it to update the view
+                        deferred.resolve(response.data);
+                    } else {
+                        // Since we don't need to do anything,
+                        // reject the deferred
+                        deferred.reject();
+                    }
+                });
+                // Return the promise from the deferred object
+                // immediately. We will eventually either
+                // resolve or reject it, but at the start, it
+                // will be in a pending state.
+                return deferred.promise;
+            },
+            getLocalTimestamp: function (staffList) {
+                var lastUpdatedLocal = "";
+                for (i = 0; i < staffList.length; i++) {
+                    // get largest (most recent) timestamp
+                    if (staffList[i].entry_date > lastUpdatedLocal) {
+                        lastUpdatedLocal = staffList[i].entry_date;
+                    }
+                }
+                return lastUpdatedLocal;
+            },
+            getRemoteTimestamp: function () {
+                return $http.get('http://www.wit.ie/api/get_channel_entries?channel_id=60&order_by=entry_date&limit=1')
+                    .success(function (data, status, headers, config) {
+                        return data;
+                    });
             }
         }
     });
